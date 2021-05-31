8acb081be524c1dc8d928e47b3f0f38eee7227a2
Duncan Britt <dbru997@gmail.com>
2021-05-30 19:42:38 -0600

    Optional custom range implemented

--- number_guesser.rb
+++ number_guesser.rb
@@ -1,10 +1,8 @@
 class GuessingGame
-  GUESS_MAX = 7
-
   def play
     loop do
       puts ''
-      puts "You have #{GUESS_MAX - guess_cnt} guesses remaining."
+      puts "You have #{guess_max - guess_cnt} guesses remaining."
       guess = input_guess
       self.guess_cnt += 1
       case guess <=> number
@@ -17,7 +15,7 @@ class GuessingGame
       when 1
         puts "You're guess is too high."
       end
-      break if guess_cnt == GUESS_MAX
+      break if guess_cnt == guess_max
     end
     puts ''
     puts 'You hvae no more guesses. You lost!'
@@ -25,7 +23,7 @@ class GuessingGame
 
   private
 
-  attr_reader :number
+  attr_reader :number, :guess_max, :beg, :last
   attr_accessor :guess_cnt
 
   def display_won
@@ -35,24 +33,111 @@ class GuessingGame
 
   def input_guess
     loop do
-      print "Enter a number between 1 and 100: "
+      print "Enter a number between #{beg} and #{last}: "
       begin
         guess = Integer(gets.chomp)
-        return guess if guess <= 100 && guess >= 1
+        return guess if guess <= last && guess >= beg
       rescue ArgumentError
       end
       print "Invalid guess. "
     end
   end
 
-  def initialize
-    @number = rand(100) + 1
+  def initialize(beg = 1, last = 100)
+    @number = rand(last - beg) + beg
+    @beg = beg
+    @last = last
     @guess_cnt = 0
+    @guess_max = Math.log2(last - beg).to_i + 1
   end
 end
 
-game = GuessingGame.new
+game = GuessingGame.new(501, 1500)
 game.play
+
+=begin
+game = GuessingGame.new(501, 1500)
+game.play
+
+You have 10 guesses remaining.
+Enter a number between 501 and 1500: 104
+Invalid guess. Enter a number between 501 and 1500: 1000
+Your guess is too low.
+
+You have 9 guesses remaining.
+Enter a number between 501 and 1500: 1250
+Your guess is too low.
+
+You have 8 guesses remaining.
+Enter a number between 501 and 1500: 1375
+Your guess is too high.
+
+You have 7 guesses remaining.
+Enter a number between 501 and 1500: 80
+Invalid guess. Enter a number between 501 and 1500: 1312
+Your guess is too low.
+
+You have 6 guesses remaining.
+Enter a number between 501 and 1500: 1343
+Your guess is too low.
+
+You have 5 guesses remaining.
+Enter a number between 501 and 1500: 1359
+Your guess is too high.
+
+You have 4 guesses remaining.
+Enter a number between 501 and 1500: 1351
+Your guess is too high.
+
+You have 3 guesses remaining.
+Enter a number between 501 and 1500: 1355
+That's the number!
+
+You won!
+
+game.play
+You have 10 guesses remaining.
+Enter a number between 501 and 1500: 1000
+Your guess is too high.
+
+You have 9 guesses remaining.
+Enter a number between 501 and 1500: 750
+Your guess is too low.
+
+You have 8 guesses remaining.
+Enter a number between 501 and 1500: 875
+Your guess is too high.
+
+You have 7 guesses remaining.
+Enter a number between 501 and 1500: 812
+Your guess is too low.
+
+You have 6 guesses remaining.
+Enter a number between 501 and 1500: 843
+Your guess is too high.
+
+You have 5 guesses remaining.
+Enter a number between 501 and 1500: 820
+Your guess is too low.
+
+You have 4 guesses remaining.
+Enter a number between 501 and 1500: 830
+Your guess is too low.
+
+You have 3 guesses remaining.
+Enter a number between 501 and 1500: 835
+Your guess is too low.
+
+You have 2 guesses remaining.
+Enter a number between 501 and 1500: 836
+Your guess is too low.
+
+You have 1 guesses remaining.
+Enter a number between 501 and 1500: 837
+Your guess is too low.
+
+You have no more guesses. You lost!
+=end
 
 =begin
 You have 7 guesses remaining.
